7d170e31fab751d1d6012605076207c46ab6dd61
Christian Schinkel <christian.schinkel@me.com>
2022-12-12 16:58:04 +0100

    Initial Commit

---
+++ OxygenTubeCalc/OxygenTubeCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  OxygenTubeCalcApp.swift
+//  OxygenTubeCalc
+//
+//  Created by Christian Schinkel on 2022-12-12.
+//
+
+import SwiftUI
+
+@main
+struct OxygenTubeCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
