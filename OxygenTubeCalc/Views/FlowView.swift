fdd61954c52e36eeaa202cf41dfd1db9ceb3c555
Christian Schinkel <christian.schinkel@me.com>
2024-09-22 21:51:33 +0200

    Addad a auge and views.

---
+++ OxygenTubeCalc/Views/FlowView.swift
@@ -0,0 +1,18 @@
+//
+//  FlowView.swift
+//  OxygenTubeCalc
+//
+//  Created by Christian Schinkel on 2024-09-22.
+//
+
+import SwiftUI
+
+struct FlowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlowView()
+}
---
+++ OxygenTubeCalc/Views/PressureView.swift
@@ -0,0 +1,18 @@
+//
+//  PressureView.swift
+//  OxygenTubeCalc
+//
+//  Created by Christian Schinkel on 2024-09-22.
+//
+
+import SwiftUI
+
+struct PressureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PressureView()
+}
